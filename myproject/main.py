645ff99cda0df4da647f64972de53ae4b4612e3e
Shreyas Choudhary <shreyas.choudhary@gmail.com>
2019-12-04 12:21:44 -0600

    's'

--- myproject/main.py
+++ myproject/main.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template,request
 import os
+
 app = Flask(__name__)
 class MyClass:
   x = 5
+  y= 10
 
 p1 = MyClass()
 print(p1.x)
@@ -12,9 +14,7 @@ def hello():
     if request.method == 'POST':
         amountDispensed = request.form.get('cups')
         print(amountDispensed)
-        os.system('ssh pi@128.194.51.251')
-        os.system('shreyasalex')
     return render_template('index.html',amount = p1)
 
 if __name__ == "__main__":
-  app.run(host = '0.0.0.0', port = '80', debug = True)
+  app.run(debug = True)
